ff43238b63443ce71f0462f3eb8fdd4296ab69ef
mateusznasewicz <mateusznasewicz@gmail.com>
2025-12-26 19:49:58 +0100

    chore: cleanup unnecessary code

--- terraform/minio.tf
+++ terraform/minio.tf
@@ -40,12 +40,3 @@ resource "minio_iam_user_policy_attachment" "developer_policy_attachment" {
   user_name   = minio_iam_user.app_user.name
   policy_name = minio_iam_policy.app_policy.name
 }
-
-resource "local_file" "minio_env" {
-  filename = "${path.module}/.env"
-  content  = <<-EOT
-    MINIO_ACCESS_KEY=${minio_iam_service_account.app_user_creds.access_key}
-    MINIO_SECRET_KEY=${minio_iam_service_account.app_user_creds.secret_key}
-    MINIO_BUCKET_NAME=${minio_s3_bucket.bucket.bucket}
-  EOT
-}
